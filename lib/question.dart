8eade795ec0957975bf692f6af58139bdc13a3c0
behzad <behzad.b.i.g@gmail.com>
2023-03-19 11:23:56 +0330

    cmt

---
+++ lib/question.dart
@@ -0,0 +1,8 @@
+class Question {
+  String questionText = 'string';
+  bool qestionAnswer = false;
+  Question({String q = 'text', bool a = false}) {
+    questionText = q;
+    qestionAnswer = a;
+  }
+}
